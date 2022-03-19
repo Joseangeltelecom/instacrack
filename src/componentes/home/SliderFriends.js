6bb777d12d57e6ffeb5e738e9e6947ab6da2f5f1
Jean Centeno <jeancenteno54@gmail.com>
2022-03-19 15:13:51 -0500

    se creo el slider de usuarios agregados

--- src/componentes/home/SliderFriends.js
+++ src/componentes/home/SliderFriends.js
@@ -1,36 +1,100 @@
 import React from "react";
 import "../../styles/home/sliderfriends.css";
 import "antd/dist/antd.css";
-import { LeftOutlined, RightOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import Glider from "react-glider";
+import "glider-js/glider.min.css";
 
 function SliderFriends() {
   return (
     <>
       <div className="carrusel">
-        <Button
-          shape="circle"
-          ghost="true"
-          icon={<LeftOutlined style={{ fontSize: "15px" }} />}
-        ></Button>
-
-        <div className="carrusel-list">
-          <div className="carrusel-item">
-            <div className="image-container">
-              <img
-                src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
-                alt="Tengen Uzui bien papi"
-              />
-              <span>tupapitengen</span>
-            </div>
+        <Glider
+          hasArrows
+          slidesToShow={5}
+          slidesToScroll={3}
+          scrollLock
+          duration={3}
+        >
+          <div className="image-container">
+            <img
+              src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
+              alt="Tengen Uzui bien papi"
+            />
+            <span>tupapitengen</span>
           </div>
-        </div>
 
-        <Button
-          shape="circle"
-          ghost="true"
-          icon={<RightOutlined style={{ fontSize: "15px" }} />}
-        ></Button>
+          <div className="image-container">
+            <img
+              src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
+              alt="Tengen Uzui bien papi"
+            />
+            <span>tupapitengen</span>
+          </div>
+
+          <div className="image-container">
+            <img
+              src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
+              alt="Tengen Uzui bien papi"
+            />
+            <span>tupapitengen</span>
+          </div>
+
+          <div className="image-container">
+            <img
+              src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
+              alt="Tengen Uzui bien papi"
+            />
+            <span>tupapitengen</span>
+          </div>
+
+          <div className="image-container">
+            <img
+              src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
+              alt="Tengen Uzui bien papi"
+            />
+            <span>tupapitengen</span>
+          </div>
+
+          <div className="image-container">
+            <img
+              src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
+              alt="Tengen Uzui bien papi"
+            />
+            <span>tupapitengen</span>
+          </div>
+
+          <div className="image-container">
+            <img
+              src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
+              alt="Tengen Uzui bien papi"
+            />
+            <span>tupapitengen</span>
+          </div>
+
+          <div className="image-container">
+            <img
+              src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
+              alt="Tengen Uzui bien papi"
+            />
+            <span>tupapitengen</span>
+          </div>
+
+          <div className="image-container">
+            <img
+              src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
+              alt="Tengen Uzui bien papi"
+            />
+            <span>tupapitengen</span>
+          </div>
+
+          <div className="image-container">
+            <img
+              src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
+              alt="Tengen Uzui bien papi"
+            />
+            <span>tupapitengen</span>
+          </div>
+        </Glider>
       </div>
 
       <div className="carrusel-indicators"></div>
