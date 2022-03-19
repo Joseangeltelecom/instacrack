8d4c9ff75b171b6324a37c95ece8e88d8aed3b74
Jean Centeno <jeancenteno54@gmail.com>
2022-03-19 01:10:21 -0500

    pre instalacion de glider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { PostPreview } from "../componentes/home/PostPreview";
+import SliderFriends from "../componentes/home/SliderFriends";
+import { UserChange } from "../componentes/home/UserChange";
 import Navbar from "../componentes/Navbar";
+import "../styles/home/home.css";
 
 function Home() {
   return (
     <div className="home-container">
       <Navbar />
-      <PostPreview />
+      <div className="post">
+        <SliderFriends />
+        <PostPreview />
+      </div>
+      <div>
+        <UserChange />
+      </div>
     </div>
   );
 }
