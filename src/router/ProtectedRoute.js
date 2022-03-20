fc840fa9642db88952170185bbefc07eb9b65d2f
Jose Angel <joseangeltelecom@gmail.com>
2022-03-20 16:42:12 -0500

    Agregando loader y habilitando nominio para autenticacion de google

--- src/router/ProtectedRoute.js
+++ src/router/ProtectedRoute.js
@@ -1,10 +1,23 @@
 import { Navigate } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
 import React from "react"
+import { Spin } from "antd"
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth()
 
-  if (loading) return <h1>Loading</h1>
+  const style = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  }
+
+  if (loading)
+    return (
+      <div style={style}>
+        <Spin color={"#123abc"} size="large" />
+      </div>
+    )
 
   if (!user) return <Navigate to="/" />
 
