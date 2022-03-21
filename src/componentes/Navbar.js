55aee5ce6701e5d648ea267769b7733cb836ce35
Jose Angel <joseangeltelecom@gmail.com>
2022-03-21 11:35:41 -0500

    Agregando dropdown para en la foto de usuario

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -6,13 +6,16 @@ import {
   regular,
   brands,
 } from "@fortawesome/fontawesome-svg-core/import.macro" // <-- import styles to be used
-import { Form, Input, Button } from "antd"
+import { Form, Input, Button, Dropdown, Menu } from "antd"
 import {
+  BookOutlined,
   HeartOutlined,
   HomeFilled,
   PlusCircleOutlined,
   SearchOutlined,
   SendOutlined,
+  SyncOutlined,
+  UserOutlined,
 } from "@ant-design/icons"
 import "../styles/navbar/navbar.css"
 
@@ -28,12 +31,46 @@ function Navbar() {
     }
   }
 
-  const style = {
-    // Adding media query..
-    "@media (max-width: 600px)": {
-      display: "none",
-    },
-  }
+  const menu = (
+    <Menu style={{ width: "200px" }}>
+      <Menu.Item>
+        <UserOutlined />
+        <a
+          className="ms-2"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com"
+        >
+          Perfil
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <BookOutlined />
+        <a
+          className="ms-2"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com"
+        >
+          Guardado
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <SyncOutlined />
+        <a
+          className="ms-2"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com"
+        >
+          Cambiar Cuenta
+        </a>
+      </Menu.Item>
+      <Menu.Item className="border-top">
+        <a onClick={handleLogout}>Salir</a>
+      </Menu.Item>
+    </Menu>
+  )
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark  fixed-top">
@@ -51,11 +88,15 @@ function Navbar() {
         </div>
         <div>
           <div clasName="form">
-            <div style={{ width: "230px" }} className="d-none d-sm-block">
+            <div
+              style={{ width: "230px", height: "35px", background: "gray" }}
+              className="d-none d-sm-block rounded"
+            >
               <Input
-                className="w-100"
+                className="w-100 h-100 rounded"
                 allowClear
                 type="text"
+                placeholder="Buscar"
                 prefix={<SearchOutlined />}
               />
             </div>
@@ -70,6 +111,18 @@ function Navbar() {
           />
           <PlusCircleOutlined className="ms-4" style={{ fontSize: "26px" }} />
           <HeartOutlined className="ms-4" style={{ fontSize: "26px" }} />
+          <Dropdown overlay={menu} placement="bottomRight" arrow>
+            <img
+              style={{
+                height: "35px",
+                width: "35px",
+                "border-radius": "50%",
+                padding: "2px",
+                marginLeft: "25px",
+              }}
+              src="https://elcomercio.pe/resizer/1AdR3_S-R4ZELHQ6WkNRGhkZhdc=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH5EJQD2ZZF5XGJM2AHNJW7HUI.jpg"
+            />
+          </Dropdown>
         </div>
       </div>
     </nav>
