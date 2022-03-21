7ff5820ba6b72b059a7c4a662298ae9d5aedbb1a
Jean Centeno <jeancenteno54@gmail.com>
2022-03-21 10:09:03 -0500

    cambios guardados

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,18 +1,25 @@
-import React from "react"
-import { Route, Routes } from "react-router-dom"
-import Chat from "../pages/Chat"
-import Home from "../pages/Home"
-import Login from "../pages/Login"
-import { Profile } from "../pages/Profile"
-import Register from "../pages/Register"
-import ResetPassword from "../pages/ResetPassword"
-import ProtectedRoute from "./ProtectedRoute"
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Chat from "../pages/Chat";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import { Profile } from "../pages/Profile";
+import Register from "../pages/Register";
+import ResetPassword from "../pages/ResetPassword";
+import ProtectedRoute from "./ProtectedRoute";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route
+        path="/register"
+        element={
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/reset" element={<ResetPassword />} />
       <Route
         path="/home"
@@ -25,5 +32,5 @@ export const AppRouter = () => {
       <Route path="/profile" element={<Profile />} />
       <Route path="/chat" element={<Chat />} />
     </Routes>
-  )
-}
+  );
+};
