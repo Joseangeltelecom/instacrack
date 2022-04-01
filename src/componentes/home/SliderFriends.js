af9fa0a37937a0f28d644819794fc24f42fb558c
Jean Centeno <jeancenteno54@gmail.com>
2022-04-01 10:42:51 -0500

    se establecieron cambios en el chatpage

--- src/componentes/home/SliderFriends.js
+++ src/componentes/home/SliderFriends.js
@@ -1,22 +1,21 @@
-import React, { useEffect, useState } from "react"
-import "../../styles/home/sliderfriends.css"
-import "antd/dist/antd.css"
-import Glider from "react-glider"
-import "glider-js/glider.min.css"
-import { collection, onSnapshot } from "firebase/firestore"
-import { useAuth } from "../../context/AuthContext"
-import { db } from "../../firebase"
-import { Link, useParams } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import "../../styles/home/sliderfriends.css";
+import "antd/dist/antd.css";
+import GliderComponent from "react-glider-carousel";
+import { collection, onSnapshot } from "firebase/firestore";
+import { useAuth } from "../../context/AuthContext";
+import { db } from "../../firebase";
+import { NavLink, useParams } from "react-router-dom";
 
 function SliderFriends() {
-  const [users, setUsers] = useState([])
-  const { user } = useAuth()
+  const [users, setUsers] = useState([]);
+  const { user } = useAuth();
   const filterUsers = users.filter((u) => {
-    return u.id !== user.currentUser.uid
-  })
+    return u.id !== user.currentUser.uid;
+  });
 
-  const params = useParams()
-  console.log(params)
+  const params = useParams();
+  console.log(params);
 
   useEffect(() => {
     const addUsersFriends = onSnapshot(collection(db, "users"), (snapshot) =>
@@ -26,41 +25,41 @@ function SliderFriends() {
           user: doc.data(),
         }))
       )
-    )
-    return () => addUsersFriends()
-  }, [])
+    );
+    return () => addUsersFriends();
+  }, []);
 
   return (
     <>
       <div className="carrusel">
-        <Glider
-          hasArrows
-          slidesToShow={5}
-          slidesToScroll={3}
-          scrollLock
-          duration={3}
-          className="d-flex"
+        <GliderComponent
+          hasArrows={true}
+          slidesToShow={4}
+          slidesToScroll={2}
+          draggable={true}
         >
-          {filterUsers.map((friend) => {
-            return (
-              <div className="image-container">
-                <Link to={`/profile/${friend.user.username}`} href="#">
-                  <img
-                    src={friend.user.imgProfile}
-                    alt="Tengen Uzui bien papi"
-                  />
-
-                  <span>{friend.user.username}</span>
-                </Link>
-              </div>
-            )
-          })}
-        </Glider>
+          {filterUsers.map((friend) => (
+            <div
+              className="glider-slide active center visible"
+              key={friend.user.username}
+            >
+              <NavLink to={`/profile/${friend.user.username}`}>
+                <img
+                  src={friend.user.imgProfile}
+                  alt="Imagen de Perfil"
+                  className="imagen"
+                />
+              </NavLink>
+              <span>{`${friend.user.username
+                .split(" ")
+                .join("")
+                .slice(0, 10)}...`}</span>
+            </div>
+          ))}
+        </GliderComponent>
       </div>
-
-      <div className="carrusel-indicators"></div>
     </>
-  )
+  );
 }
 
-export default SliderFriends
+export default SliderFriends;
